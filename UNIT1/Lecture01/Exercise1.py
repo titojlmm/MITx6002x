21632e1d85d0527f6bfda10ffe91a0f961351161
José Luis Martín Muñoz <titojlmm@gmail.com>
2020-04-01 21:00:54 +0200

    Ejercicios de la primera lección

---
+++ UNIT1/Lecture01/Exercise1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def metric1(item):
+    return item.getValue() / item.getWeight() 
+
+
+def metric2(item):
+    return  -item.getWeight()
+
+
+def metric3(item):
+    return item.getValue()
+
---
+++ UNIT1/Lecture01/Exercise2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+item	 $	  kg	$/kg
+clock	 175  10	17.5
+picture	 90	  9	    10
+radio	 20	  4  	5
+vase	 50	  2	    25
+book	 10	  1	    10
+computer 200  20	10
+"""
+
+# MAXIMUM WEIGHT: 20
+
+# Metric: max value
+"""
+  First: computer
+  Second: none
+  Total value: 200
+  Total weight: 20
+"""
+
+# Metric: min weight
+"""
+  First: book
+  Second: vase
+  Total value: 60
+  Total weight: 3
+"""
+
+# Metric: max value/weight ratio 
+"""
+  First: vase
+  Second: clock
+  Total value: 225
+  Total weight: 12
+"""
